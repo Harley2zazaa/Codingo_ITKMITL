1b3183c7849ade8d6074a29a93e986090a4a85ee
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-03-11 13:04:45 +0700

    updated run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 cd /d "%~dp0"
 
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :3000') do taskkill /f /pid %%a
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :3000 ^| findstr "LISTENING"') do taskkill /f /pid %%a
 
 start "" cmd /c "timeout /t 2 /nobreak >nul && start http://localhost:3000"
 
