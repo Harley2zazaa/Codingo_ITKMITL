7e7ea4a0e4534a1a591b57107b78856d732a5d01
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-03-11 13:00:23 +0700

    updated run.bat

--- run.bat
+++ run.bat
@@ -2,11 +2,9 @@
 
 cd /d "%~dp0"
 
-set PORT=3000
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr :3000') do taskkill /f /pid %%a
 
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":%PORT%" ^| findstr "LISTENING"') do (
-    taskkill /F /PID %%a
-)
+start "" cmd /c "timeout /t 2 /nobreak >nul && start http://localhost:3000"
 
 npx nodemon index.js
 
