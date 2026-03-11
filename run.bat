9cc5af503c3c769d7d28a246efdc4cdbbbcddc9a
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-03-11 12:47:20 +0700

    update run.bat

--- run.bat
+++ run.bat
@@ -2,26 +2,12 @@
 
 cd /d "%~dp0"
 
-node --version >nul 2>&1
-if %errorlevel% neq 0 (
-    pause
-    exit /b 1
-)
-
-if not exist package.json (
-    npm init -y
-)
-
-if not exist node_modules (
-    npm install nodemon express ejs sqlite3 express-session
-)
-
 set PORT=3000
 
 for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":%PORT%" ^| findstr "LISTENING"') do (
     taskkill /F /PID %%a
 )
 
-nodemon index.js
+npx nodemon index.js
 
 pause
